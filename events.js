3d51a33ec8f1ba26cda81113ca6c9fdf1ea623a6
RaulDM <raul.delgado@hotmail.com>
2025-02-08 00:54:20 +0100

    #05-AMR

--- events.js
+++ events.js
@@ -35,72 +35,129 @@ for (let i = 0; i < btnTripClose.length; ++i) {
 // Evento carrousel sobre las tarjetas populares
 
 document.addEventListener("DOMContentLoaded", function () {
-  let current_exp = 0;
-  const POSICION = document.querySelectorAll(".card-exp");
-  // Variables para almacenar las flechas
-  let al = document.getElementById("arrow-left");
-  let ar = document.getElementById("arrow-right");
+  if (window.location.pathname.includes("index.html")) {
+    let current_exp = 0;
+    const POSICION = document.querySelectorAll(".card-exp");
+    // Variables para almacenar las flechas
+    let al = document.getElementById("arrow-left");
+    let ar = document.getElementById("arrow-right");
 
+    POSICION[current_exp].style.opacity = "1";
 
+    document.getElementById("arrow-right").onclick = function () {
+      // Creamos una condición para desplazar la tarjeta visible en pantalla y que aparezca la que se encuentra a su izquierda. Entra en la condición si el valor de la tarjeta actual es inferior la cantidad máxima de tarjeta que disponemos.
+      if (current_exp < 3) {
+        // Desplazamos la tarjeta visible a la derecha y bajamos su opacidad.
+        POSICION[current_exp].style.left = "110%";
+        POSICION[current_exp].style.opacity = "0";
+        // Sumamos 1 para que la tarjeta activa pase a la siguiente.
+        current_exp++;
+        // Desplazamos la siguiente tarjeta hacia la derecha y subimos la opacidad.
+        POSICION[current_exp].style.left = "15%";
+        POSICION[current_exp].style.opacity = "1";
+        // Nos aseguramos que el botón con flecha hacia la derecha siga activo.
+        ar.style.display = "initial";
+      }
+      // Creamos condición para que cuando hayamos llegado a la última tarjeta disponible, ya no se pueda tener la opción de desplazar hacia la derecha
+      if (current_exp === 3) {
+        console.log("AQUI");
+        ar.style.display = "none";
+      }
+      al.style.display = "initial";
+      // carrousel();
+    };
 
-  POSICION[current_exp].style.opacity = '1'
+    document.getElementById("arrow-left").onclick = function () {
+      // Creamos una condición para desplazar la tarjeta visible en pantalla y que aparezca la que se encuentra a su izquierda. Entra en la condición si el valor de la tarjeta actual es inferior la cantidad máxima de tarjeta que disponemos.
+      console.log(current_exp);
+      if (current_exp > 0) {
+        // Desplazamos la tarjeta visible a la izquierda y bajamos la opacidad.
+        POSICION[current_exp].style.left = "-110%";
+        POSICION[current_exp].style.opacity = "0";
 
-  function checkWidth () {
-    if(document.documentElement.clientWidth < 1000) {
-      POSICION[current_exp].style.left = '15%'
+        // Restamos 1 para que la tarjeta activa pase a la anterior.
+        current_exp--;
+        // Desplazamos la siguiente tarejta hacia la izquierda.
+        POSICION[current_exp].style.left = "15%";
+        POSICION[current_exp].style.opacity = "1";
+        // Nos aseguramos que el botón con flecha hacia la izquierda siga activo.
+        ar.style.display = "initial";
+      }
+      // Creamos condición para que cuando hayamos llegado a la última tarjeta disponible, ya no se pueda tener la opción de desplazar hacia la izquierda
+      if (current_exp === 0) {
+        console.log("AQUI");
+        al.style.display = "none";
+      }
+    };
+
+    // Cuando la pantalla llegue a los 1000px, desaparece el efecto carrousel
+    // Al hacer ello la primera tarjeta queda desplazada left:15% indicado nada mas cargar la página.
+    // Hay que crear una función para que cuando la pantalla llegue a ese tamaño, deje de aplicar esa propiedad.
+
+    function controlWidth() {
+      const ANCHO = window.innerWidth;
+      console.log(ANCHO);
+
+      if (ANCHO < 1000) {
+        POSICION[0].style.left = "15%";
+      } else {
+        POSICION[0].style.left = 0;
+      }
     }
+
+    //Agregar el evento de cambio de tamaño de la pantalla
+    window.addEventListener("resize", controlWidth);
+
+    controlWidth();
   }
-  
-
-
-
-
-  document.getElementById("arrow-right").onclick = function () {
-    // Creamos una condición para desplazar la tarjeta visible en pantalla y que aparezca la que se encuentra a su izquierda. Entra en la condición si el valor de la tarjeta actual es inferior la cantidad máxima de tarjeta que disponemos.
-    if (current_exp < 3) {
-      // Desplazamos la tarjeta visible a la derecha y bajamos su opacidad.
-      POSICION[current_exp].style.left = "110%";
-      POSICION[current_exp].style.opacity = "0";
-      // Sumamos 1 para que la tarjeta activa pase a la siguiente.
-      current_exp++;
-      // Desplazamos la siguiente tarjeta hacia la derecha y subimos la opacidad.
-      POSICION[current_exp].style.left = "15%";
-      POSICION[current_exp].style.opacity = "1";
-      // Nos aseguramos que el botón con flecha hacia la derecha siga activo.
-      ar.style.display = "initial";
-    }
-    // Creamos condición para que cuando hayamos llegado a la última tarjeta disponible, ya no se pueda tener la opción de desplazar hacia la derecha
-    if (current_exp === 3) {
-      console.log("AQUI");
-      ar.style.display = "none";
-    }
-    al.style.display = "initial";
-    // carrousel();
-  };
-
-  document.getElementById("arrow-left").onclick = function () {
-    // Creamos una condición para desplazar la tarjeta visible en pantalla y que aparezca la que se encuentra a su izquierda. Entra en la condición si el valor de la tarjeta actual es inferior la cantidad máxima de tarjeta que disponemos.
-    console.log(current_exp)
-    if (current_exp > 0) {
-      // Desplazamos la tarjeta visible a la izquierda y bajamos la opacidad.
-      POSICION[current_exp].style.left = "-110%";
-      POSICION[current_exp].style.opacity = "0";
-
-      // Sumamos 1 para que la tarjeta activa pase a la siguiente.
-      current_exp--;
-      // Desplazamos la siguiente tarejta hacia la izquierda. 
-      POSICION[current_exp].style.left = "15%"; 
-      POSICION[current_exp].style.opacity = "1";
-      // Nos aseguramos que el botón con flecha hacia la izquierda siga activo.
-      ar.style.display = "initial";
-    }
-    // Creamos condición para que cuando hayamos llegado a la última tarjeta disponible, ya no se pueda tener la opción de desplazar hacia la izquierda
-    if (current_exp === 0) {
-      console.log("AQUI");
-      al.style.display = "none";
-    }
-
-  };
-
 });
 
+/*---------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+
+// Evento cuando hacemos scroll en el blog y vayan apareciendo los articulos.
+
+// verificar la URL del archivo actual antes de ejecutar el evento
+
+// Almacenamos en una constante todos los articulos
+const ARTICULOS = document.querySelectorAll(".art-blog");
+
+// Creamos variable para almacenar la altura máxima que nos servirá para un bucle for
+let hw = window.innerHeight;
+console.log(hw);
+
+// Variable para poder ir acceder a cada uno de los elementos (ARTICULOS[article_actual])
+//let article_actual = 0;
+
+function aparecerArticulo() {
+  ARTICULOS.forEach((article) => {
+    // guardo en una variable la posición top del articulo.
+    const POSICION = article.getBoundingClientRect().top;
+    const APARECER = hw - hw / 5;
+
+    console.log("posicion:" + POSICION + "aparacer: " + APARECER);
+
+    if (POSICION < APARECER) {
+      console.log("AHORA");
+      article.style.opacity = "1";
+      article.style.transform = "scale(1)";
+    }
+  });
+  // for (let i = 0; i < hw; i++) {
+  //   let position = Math.floor(
+  //     ARTICULOS[article_actual].getBoundingClientRect().x
+  //   );
+  //   console.log(position);
+  //   if (i === position) {
+  //     console.log("entra");
+  //     ARTICULOS[article_actual].style.transform = "scale(1)";
+  //     ARTICULOS[article_actual].style.opacity = "1";
+  //   }
+  //   article_actual++;
+  // }
+}
+if (window.location.pathname.includes("blog.html")) {
+  window.addEventListener("scroll", aparecerArticulo);
+}
